248fcbdfbff5d9fc43674adf0c9b45a454b52352
刘鹏 <lp_aideals@hotmail.com>
2016-09-20 21:19:08 +0800

    Initial Commit

---
+++ Test_delegate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test_delegate
+//
+//  Created by 鹏 刘 on 16/9/20.
+//  Copyright © 2016年 鹏 刘. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
